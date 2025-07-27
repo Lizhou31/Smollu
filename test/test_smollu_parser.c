11c66da32494a669be4f95a1547d89cc518cb5e3
Paul <lisie31s@gmail.com>
2025-07-27 22:37:05 +0800

    [Test] Add unit tests of function call and native call.

--- test/test_smollu_parser.c
+++ test/test_smollu_parser.c
@@ -26,13 +26,21 @@ static ASTNode *init_first_stmt(ASTNode *root) {
 Test(parser, precedence) {
     const char *code =
         "init {}\n"
-        "main { 1 + 2 * 3; }";
+        "main { \n"
+        "  1 + 2 * 3; \n"
+        "  (1 + 2) * 3; \n"
+        "  1 + 2 * 3 / 4; \n"
+        "}"
+        "functions {\n"
+        "}";
     Lexer lex; lexer_init(&lex, code);
     Parser p; parser_init(&p, &lex);
 
     ASTNode *root = parse_program(&p);
 
     ASTNode *stmt = main_first_stmt(root);
+
+    /* x = 1 + 2 * 3; */
     expect_node_type(stmt, AST_BINARY, "top binary");
     cr_assert_eq(stmt->as.binary.op, TOK_PLUS, "Top op should be +");
 
@@ -51,6 +59,56 @@ Test(parser, precedence) {
     expect_node_type(r_right, AST_INT_LITERAL, "nested right");
     cr_assert_eq(r_right->as.int_val, 3);
 
+    /* y = (1 + 2) * 3; */
+    stmt = stmt->next;
+    expect_node_type(stmt, AST_BINARY, "top binary");
+    cr_assert_eq(stmt->as.binary.op, TOK_STAR, "Top op should be *");
+
+    left = stmt->as.binary.left;
+    expect_node_type(left, AST_BINARY, "left binary");
+    cr_assert_eq(left->as.binary.op, TOK_PLUS, "Left op should be +");
+
+    ASTNode *l_left = left->as.binary.left;
+    ASTNode *l_right = left->as.binary.right;
+    expect_node_type(l_left, AST_INT_LITERAL, "nested left");
+    cr_assert_eq(l_left->as.int_val, 1);
+    expect_node_type(l_right, AST_INT_LITERAL, "nested right");
+    cr_assert_eq(l_right->as.int_val, 2);
+
+    right = stmt->as.binary.right;
+    expect_node_type(right, AST_INT_LITERAL, "right operand");
+    cr_assert_eq(right->as.int_val, 3);
+
+
+    /* z = 1 + 2 * 3 / 4; */
+    stmt = stmt->next;
+    expect_node_type(stmt, AST_BINARY, "top binary");
+    cr_assert_eq(stmt->as.binary.op, TOK_PLUS, "Top op should be +");
+
+    left = stmt->as.binary.left;
+    expect_node_type(left, AST_INT_LITERAL, "left operand");
+    cr_assert_eq(left->as.int_val, 1);
+
+    right = stmt->as.binary.right;
+    expect_node_type(right, AST_BINARY, "right binary");
+    cr_assert_eq(right->as.binary.op, TOK_SLASH);
+
+    r_right = right->as.binary.right;
+    expect_node_type(r_right, AST_INT_LITERAL, "nested left");
+    cr_assert_eq(r_right->as.int_val, 4);
+
+
+    r_left = right->as.binary.left;
+    expect_node_type(r_left, AST_BINARY, "nested right");
+    cr_assert_eq(r_left->as.binary.op, TOK_STAR);
+
+    ASTNode *r_l_left = r_left->as.binary.left;
+    ASTNode *r_l_right = r_left->as.binary.right;
+    expect_node_type(r_l_left, AST_INT_LITERAL, "nested left");
+    cr_assert_eq(r_l_left->as.int_val, 2);
+    expect_node_type(r_l_right, AST_INT_LITERAL, "nested right");
+    cr_assert_eq(r_l_right->as.int_val, 3);
+
     /* Cleanup */
     ast_free(root);
     parser_free(&p);
@@ -64,6 +122,8 @@ Test(parser, while_and_assignment) {
         "}\n"
         "main {\n"
         "  while (x < 5) { x = x + 1; }\n"
+        "}"
+        "functions {\n"
         "}";
 
     Lexer lex; lexer_init(&lex, code);
@@ -103,7 +163,7 @@ Test(parser, while_and_assignment) {
     ast_free(root);
     parser_free(&p);
     lexer_free(&lex);
-} 
+}
 
 Test(parser, if_elif_else) {
     const char *code =
@@ -112,6 +172,8 @@ Test(parser, if_elif_else) {
         "  if (x < 3) { x = x + 1; }\n"
         "  elif (x < 6) { x = x + 2; }\n"
         "  else { x = x - 1; }\n"
+        "}"
+        "functions {\n"
         "}";
 
     Lexer lex; lexer_init(&lex, code);
@@ -160,6 +222,121 @@ Test(parser, if_elif_else) {
     ASTNode *else_body_stmt_stmt = else_body_stmt->as.block.stmts;
     expect_node_type(else_body_stmt_stmt, AST_ASSIGNMENT, "else body assignment");
     cr_assert_str_eq(else_body_stmt_stmt->as.assign.name, "x");
+
+    /* Cleanup */
+    ast_free(root);
+    parser_free(&p);
+    lexer_free(&lex);
+} 
+
+Test(parser, function_definition) {
+    const char *code =
+        "init {}\n"
+        "main {}\n"
+        "functions {\n"
+        "  function add(a, b) {\n"
+        "    local sum = a + b;\n"
+        "    x = sum;\n"
+        "  }\n"
+        "  function greet(name) {\n"
+        "    native print(name);\n"
+        "  }\n"
+        "}";
+
+    Lexer lex; lexer_init(&lex, code);
+    Parser p; parser_init(&p, &lex);
+
+    ASTNode *root = parse_program(&p);
+
+    /* Check functions section */
+    ASTNode *functions = root->as.program.functions;
+    expect_node_type(functions, AST_BLOCK, "functions block");
+
+    /* First function: add */
+    ASTNode *func1 = functions->as.block.stmts;
+    expect_node_type(func1, AST_FUNCTION_DEF, "first function def");
+    cr_assert_str_eq(func1->as.func_def.name, "add");
+
+    /* Check parameters */
+    ASTNode *param1 = func1->as.func_def.params;
+    expect_node_type(param1, AST_PARAMETER_LIST, "first parameter");
+    cr_assert_str_eq(param1->as.param.param_name, "a");
+    
+    ASTNode *param2 = param1->next;
+    expect_node_type(param2, AST_PARAMETER_LIST, "second parameter");
+    cr_assert_str_eq(param2->as.param.param_name, "b");
+
+    /* Check function body */
+    ASTNode *body = func1->as.func_def.body;
+    expect_node_type(body, AST_BLOCK, "function body");
+
+    /* Second function: greet */
+    ASTNode *func2 = func1->next;
+    expect_node_type(func2, AST_FUNCTION_DEF, "second function def");
+    cr_assert_str_eq(func2->as.func_def.name, "greet");
+
+    /* Cleanup */
+    ast_free(root);
+    parser_free(&p);
+    lexer_free(&lex);
+}
+
+Test(parser, native_and_function_calls) {
+    const char *code =
+        "init {\n"
+        "  native setup_gpio(pin, mode);\n"
+        "  configure_system();\n"
+        "}\n"
+        "main {\n"
+        "  local result = calculate(x, y);\n"
+        "  native print(result);\n"
+        "  process_data(result, 42);\n"
+        "}\n"
+        "functions {}";
+
+    Lexer lex; lexer_init(&lex, code);
+    Parser p; parser_init(&p, &lex);
+
+    ASTNode *root = parse_program(&p);
+
+    /* Check init section */
+    ASTNode *init = root->as.program.init;
+    ASTNode *init_stmt1 = init->as.block.stmts;
+    
+    /* First statement: native call */
+    expect_node_type(init_stmt1, AST_NATIVE_CALL, "native call");
+    cr_assert_str_eq(init_stmt1->as.native_call.name, "setup_gpio");
+    
+    /* Check arguments */
+    ASTNode *arg1 = init_stmt1->as.native_call.args;
+    expect_node_type(arg1, AST_IDENTIFIER, "first argument");
+    ASTNode *arg2 = arg1->next;
+    expect_node_type(arg2, AST_IDENTIFIER, "second argument");
+
+    /* Second statement: function call */
+    ASTNode *init_stmt2 = init_stmt1->next;
+    expect_node_type(init_stmt2, AST_FUNCTION_CALL, "function call");
+    cr_assert_str_eq(init_stmt2->as.func_call.name, "configure_system");
+
+    /* Check main section */
+    ASTNode *main = root->as.program.main;
+    ASTNode *main_stmt1 = main->as.block.stmts;
+
+    /* First statement: assignment with function call */
+    expect_node_type(main_stmt1, AST_ASSIGNMENT, "assignment");
+    ASTNode *assign_value = main_stmt1->as.assign.value;
+    expect_node_type(assign_value, AST_FUNCTION_CALL, "function call in assignment");
+    cr_assert_str_eq(assign_value->as.func_call.name, "calculate");
+
+    /* Second statement: native call */
+    ASTNode *main_stmt2 = main_stmt1->next;
+    expect_node_type(main_stmt2, AST_NATIVE_CALL, "native call");
+    cr_assert_str_eq(main_stmt2->as.native_call.name, "print");
+
+    /* Third statement: function call */
+    ASTNode *main_stmt3 = main_stmt2->next;
+    expect_node_type(main_stmt3, AST_FUNCTION_CALL, "function call");
+    cr_assert_str_eq(main_stmt3->as.func_call.name, "process_data");
 
     /* Cleanup */
     ast_free(root);
