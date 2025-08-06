c0cd714e2493647cc7d018481e6286edd7dc4cb9
Paul <lisie31s@gmail.com>
2025-08-07 00:31:25 +0800

    [Fix] Fix include error.

--- src/components/compiler/smollu_compiler.h
+++ src/components/compiler/smollu_compiler.h
@@ -17,6 +17,8 @@ extern "C" {
 #endif
 
 #include <stddef.h> /* for size_t */
+#include <stdint.h> /* for uint8_t */
+#include <stdio.h>  /* for FILE */
 
 /* ──────────────────────────────────────────────────────────────────────────── */
 /*  Token types                                                                */
